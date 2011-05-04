66eaf8d056a076144bca9dd5fb1f6a4dfd8cfd61
Philipp Klose <me@thehippo.de>
2011-05-04 20:01:49 +0200

    clarified license

--- hxjson2/JSONParseError.hx
+++ hxjson2/JSONParseError.hx
@@ -1,13 +1,6 @@
 /*
-  hxjson2 by Philipp Klose (philipp.klose@byteanvil.com)
-  
-  Ported from as3corelib (http://code.google.com/p/as3corelib/)
-  
-  com.adobe.serialization.JSONParseError
-  
-  Original source code by:
-  
   Copyright (c) 2008, Adobe Systems Incorporated
+  Copyright (c) 2011, Philipp Klose
   All rights reserved.
 
   Redistribution and use in source and binary forms, with or without 
@@ -15,15 +8,15 @@
   met:
 
   * Redistributions of source code must retain the above copyright notice, 
-    this list of conditions and the following disclaimer.
-  
+  this list of conditions and the following disclaimer.
+
   * Redistributions in binary form must reproduce the above copyright
-    notice, this list of conditions and the following disclaimer in the 
-    documentation and/or other materials provided with the distribution.
-  
+  notice, this list of conditions and the following disclaimer in the 
+  documentation and/or other materials provided with the distribution.
+
   * Neither the name of Adobe Systems Incorporated nor the names of its 
-    contributors may be used to endorse or promote products derived from 
-    this software without specific prior written permission.
+  contributors and authors may be used to endorse or promote products
+  derived from this software without specific prior written permission.
 
   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
   IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
